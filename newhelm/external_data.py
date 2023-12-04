ba92f2cfab8c58d323e24f49f0af1e832984da23
Brian Goldman <auxy@google.com>
2023-12-04 09:06:03 -0700

    Clean up test file and add a few missing doc strings.

--- newhelm/external_data.py
+++ newhelm/external_data.py
@@ -4,12 +4,16 @@ from newhelm.general import shell
 
 
 class ExternalData(ABC):
+    """Base class for defining a source of external data."""
+
     @abstractmethod
     def download(self, location):
         pass
 
 
 class WebData(ExternalData):
+    """External data that can be trivially downloaded using wget."""
+
     def __init__(self, source_url):
         self.source_url = source_url
 
