7875a3bae1a6c46f31c0e3b3c1bbc9228479d86a
Brian Goldman <auxy@google.com>
2023-12-14 14:22:37 -0700

    Backporting changes

--- newhelm/demo_loading_benchmark.py
+++ newhelm/demo_loading_benchmark.py
@@ -1,13 +1,13 @@
 from typing import List
 from newhelm.benchmark import BaseBenchmark
 from newhelm.load_plugins import load_plugins
-from newhelm.general import get_all_subclasses
+from newhelm.general import get_concrete_subclasses
 
 
 if __name__ == "__main__":
     load_plugins()
     all_benchmarks: List[BaseBenchmark] = [
-        cls() for cls in get_all_subclasses(BaseBenchmark)  # type: ignore[type-abstract]
+        cls() for cls in get_concrete_subclasses(BaseBenchmark)  # type: ignore[type-abstract]
     ]
     for benchmark in all_benchmarks:
         print(benchmark.__class__.__name__)
