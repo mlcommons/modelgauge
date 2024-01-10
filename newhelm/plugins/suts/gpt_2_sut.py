567fb2eb0bdf794afcc51e9337c7f5579b9b4a6b
Brian Goldman <auxy@google.com>
2024-01-10 11:31:43 -0700

    Fully working GPT2 using HELM's huggingface client.
    I've stripped out caching and the server factory.

--- newhelm/plugins/suts/gpt_2_sut.py
+++ newhelm/plugins/suts/gpt_2_sut.py
@@ -1,4 +1,5 @@
 from typing import List
+from newhelm.huggingface_client import HuggingFaceClient, Request, RequestResult
 from newhelm.placeholders import Prompt
 from newhelm.sut import SUTResponse, PromptResponseSUT
 
@@ -6,7 +7,21 @@ from newhelm.sut import SUTResponse, PromptResponseSUT
 class GPT2(PromptResponseSUT):
     """The SUT should have all the details currently spread across model_deployment and model_metadata."""
 
+    def __init__(self):
+        self.model = HuggingFaceClient("gpt2")
+
     def evaluate(self, prompt: Prompt) -> SUTResponse:
-        # Pure placeholder.
-        number_of_words = len(prompt.text.split())
-        return SUTResponse(f"The prompt has {number_of_words} words.")
+        response: RequestResult = self.model.make_request(
+            Request(
+                prompt=prompt.text,
+                # Configured to match BBQ right now.
+                temperature=0.0,
+                num_completions=1,
+                top_k_per_token=5,
+                max_tokens=1,
+                stop_sequences=[],
+                echo_prompt=False,
+                top_p=1,
+            )
+        )
+        return SUTResponse(response.completions[0].text)
