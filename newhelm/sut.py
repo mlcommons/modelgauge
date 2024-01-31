83499fec53271b4f1c39de9a4c1ec92323fc4242
Brian Goldman <auxy@google.com>
2024-01-31 08:56:55 -0700

    Reintroduce the idea of multiple completions in a single SUT response.
    This exists in hugging face, OpenAI, and Together, and is required by RealToxicityPrompts. Most of the refactor here is actually in PerspectiveAPI, which required some updates to how it does batching.

--- newhelm/sut.py
+++ newhelm/sut.py
@@ -9,10 +9,17 @@ ResponseType = TypeVar("ResponseType")
 
 
 @dataclass(frozen=True)
+class Completion:
+    """All data about a single completion in the response."""
+
+    text: str
+
+
+@dataclass(frozen=True)
 class SUTResponse:
     """The data that came out of the SUT."""
 
-    completion: str
+    completions: List[Completion]
 
 
 class SUT(ABC):
