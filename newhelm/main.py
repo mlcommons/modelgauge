5a3abf7395209f3b3b56e684a0f67ec0545f141e
Brian Goldman <auxy@google.com>
2024-02-01 14:48:06 -0700

    Fix bad merge

--- newhelm/main.py
+++ newhelm/main.py
@@ -56,26 +56,6 @@ def run_sut(sut: str, secrets: str, prompt: str):
     print(result, "\n")
 
 
-# TODO: Consider moving this somewhere else.
-@newhelm_cli.command()
-@SUT_OPTION
-@SECRETS_FILE_OPTION
-@click.option("--prompt", help="The full text to send to the SUT.")
-def run_sut(sut: str, secrets: str, prompt: str):
-    sut_obj = SUTS.make_instance(sut)
-    SECRETS.set_values(get_or_create_json_file(secrets))
-    # Current this only knows how to do prompt response, so assert that is what we have.
-    assert isinstance(sut_obj, PromptResponseSUT)
-
-    prompt_obj = Prompt(prompt)
-    request = sut_obj.translate_request(prompt_obj)
-    print(request, "\n")
-    response = sut_obj.evaluate(request)
-    print(response, "\n")
-    result = sut_obj.translate_response(prompt_obj, response)
-    print(result, "\n")
-
-
 if __name__ == "__main__":
     load_plugins()
     newhelm_cli()
