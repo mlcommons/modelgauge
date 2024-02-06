1e1bf36e9fb026befc402d58e91f773c4806dde6
Mike Lay <mike@mkly.io>
2024-02-06 14:35:12 -0800

    Cleanup tests
    * Set origin and destination file names to prevent missing possible
      argument mismatch
    * Inline `mocker` to reduce noise level

--- tests/test_external_data.py
+++ tests/test_external_data.py
@@ -1,25 +1,15 @@
-import pytest
-
 from newhelm.external_data import WebData, LocalData
 
 
-@pytest.fixture
-def mock_download(mocker):
-    return mocker.patch("urllib.request.urlretrieve")
-
-
-@pytest.fixture
-def mock_copy(mocker):
-    return mocker.patch("shutil.copy")
-
-
-def test_web_data_download(mock_download):
+def test_web_data_download(mocker):
+    mock_download = mocker.patch("urllib.request.urlretrieve")
     web_data = WebData(source_url="http://example.com")
     web_data.download("test.tgz")
     mock_download.assert_called_once_with("http://example.com", "test.tgz")
 
 
-def test_local_data_download(mock_copy):
-    local_data = LocalData(path="test.tgz")
-    local_data.download("test.tgz")
-    mock_copy.assert_called_once_with("test.tgz", "test.tgz")
+def test_local_data_download(mocker):
+    mock_copy = mocker.patch("shutil.copy")
+    local_data = LocalData(path="origin_test.tgz")
+    local_data.download("destintation_test.tgz")
+    mock_copy.assert_called_once_with("origin_test.tgz", "destintation_test.tgz")
