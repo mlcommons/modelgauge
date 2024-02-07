59c053f130732f9f119e410add5e7c44d0451169
Mike Lay <mike@mkly.io>
2024-02-07 10:39:08 -0800

    Use union

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Any, List, Generic, TypeVar
+from typing import List
 from pydantic import BaseModel
 
 
@@ -15,17 +15,15 @@ class Derived2(SomeBase):
     field_2: int
 
 
-TSomeBase = TypeVar("TSomeBase", bound="SomeBase", covariant=True)
 
-
-class Wrapper(BaseModel, Generic[TSomeBase]):
-    elements: List[TSomeBase]
-    any_union: Any
+class Wrapper(BaseModel):
+    elements: List[Derived1 | Derived2]
+    any_union: Derived1 | Derived2
 
 
 def test_pydantic_lack_of_polymorphism_serialize():
     """This test is showing that Pydantic doesn't serialize like we want."""
-    wrapper = Wrapper[Derived1 | Derived2](
+    wrapper = Wrapper(
         elements=[Derived1(all_have=20, field_1=1), Derived2(all_have=20, field_2=2)],
         any_union=Derived1(all_have=30, field_1=3),
     )
@@ -42,8 +40,8 @@ def test_pydantic_lack_of_polymorphism_deserialize():
         """{"elements":[{"all_have":20, "field_1": 1},{"all_have":20, "field_2": 2}],"any_union":{"all_have":30,"field_1":3}}""",
         strict=True,
     )
-    # These should be Derived1 and Derived2
-    assert from_json.elements[0].__class__.__name__ == "SomeBase"
-    assert from_json.elements[1].__class__.__name__ == "SomeBase"
+    # These is now Derived1 and Derived2
+    assert from_json.elements[0].__class__.__name__ == "Derived1"
+    assert from_json.elements[1].__class__.__name__ == "Derived2"
     # This should be Derived1
     assert from_json.any_union.__class__.__name__ == "dict"
