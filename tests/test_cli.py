f551d8f12edf16cc32b3a632db6ef35317c66e55
Mike Lay <mike@mkly.io>
2024-02-12 14:33:57 -0800

    Add `--expensive-tests` pytest option
    * Add `--expensive-tests` pytest option to exclude expensive cli tests
      from `pytest`
    * Remove previous exclusion solution

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,21 +3,25 @@ import pathlib
 
 import pytest
 
+expensive_tests = pytest.mark.skipif("not config.getoption('expensive-tests')")
+
 
 @pytest.fixture
 def cmd():
     return pathlib.Path(__file__).parent.parent / "newhelm" / "main.py"
 
 
+@expensive_tests
 def test_main(cmd):
     assert os.system(f"python {cmd}") == 0
 
 
+@expensive_tests
 def test_list_plugins(cmd):
     assert os.system(f"python {cmd} list") == 0
 
 
-@pytest.mark.requires_plugins
+@expensive_tests
 @pytest.mark.parametrize("sut", ["DemoMultipleChoiceSUT"])
 @pytest.mark.parametrize("test", ["demo_01", "demo_02"])
 def test_test_sut_combinations(cmd, test, sut):
