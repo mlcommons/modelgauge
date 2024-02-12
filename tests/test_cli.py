ed225e663aa6cf3611266dc573fdafd91c1716e8
Mike Lay <mike@mkly.io>
2024-02-12 12:17:24 -0800

    Add small cli test to catch regressions
    * Add small cli test to catch regressions
    
    This would catch the command failure currently with "demo_03". Which is
    why "demo_03" is not currently added to the matrix.
    
    "gpt2" can probably also be added to the matrix as a sut, but wanted to
    keep this simple as a first pass.

---
+++ tests/test_cli.py
@@ -0,0 +1,31 @@
+import os
+import pathlib
+
+import pytest
+
+
+@pytest.fixture
+def cmd():
+    return pathlib.Path(__file__).parent.parent / "newhelm" / "main.py"
+
+
+def test_main(cmd):
+    assert os.system(f"python {cmd}") == 0
+
+
+def test_list_plugins(cmd):
+    assert os.system(f"python {cmd} list") == 0
+
+
+@pytest.mark.parametrize("sut", ["DemoMultipleChoiceSUT"])
+@pytest.mark.parametrize("test", ["demo_01", "demo_02"])
+def test_test_sut_combinations(cmd, test, sut):
+    assert (
+        os.system(
+            f"""python {cmd} run-test \
+                --test {test} \
+                --sut {sut} \
+                --max-test-items 1"""
+        )
+        == 0
+    )
