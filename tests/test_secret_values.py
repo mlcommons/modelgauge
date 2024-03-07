b913be9bbc09bfcb31ad0dc204d48ff236421dfd
Brian Goldman <auxy@google.com>
2024-03-07 13:00:03 -0700

    Stash more tests

--- tests/test_secret_values.py
+++ tests/test_secret_values.py
@@ -1,6 +1,8 @@
+from newhelm.general import get_class
 from newhelm.secret_values import (
     RequiredSecret,
     SecretDescription,
+    SerializedSecret,
     get_all_secrets,
 )
 
@@ -16,7 +18,19 @@ class MySecret(RequiredSecret):
 def test_get_all_secrets():
     descriptions = get_all_secrets()
     test_secret = SecretDescription(
-            scope="some-scope", key="some-ke", instructions="some-instructions"
-        )
-    matching = [s for s in test_secret if s == test_secret]
+        scope="some-scope", key="some-key", instructions="some-instructions"
+    )
+    matching = [s for s in descriptions if s == test_secret]
     assert len(matching) == 1, f"Found secrets: {descriptions}"
+
+
+def test_serialize_secret():
+    original = MySecret("some-value")
+    serialized = SerializedSecret.serialize(original)
+    assert serialized == SerializedSecret(
+        module="test_secret_values", qual_name="MySecret"
+    )
+    returned = get_class(serialized.module, serialized.qual_name)
+    assert returned.description() == SecretDescription(
+        scope="some-scope", key="some-key", instructions="some-instructions"
+    )
