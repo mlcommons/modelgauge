03064c269719ab132c200fc1a510b073f2a34889
Barbara Korycki <barbara@mlcommons.org>
2024-03-08 14:45:06 -0500

    New GDrive ExternalData class to handle downloading cvs from a folder in SafeTest

--- newhelm/external_data.py
+++ newhelm/external_data.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Optional
 import shutil
 import urllib.request
+import gdown  # type: ignore
 
 from newhelm.data_packing import DataDecompressor, DataUnpacker
 from newhelm.general import UrlRetrieveProgressBar
@@ -31,6 +32,27 @@ class WebData(ExternalData):
             self.source_url,
             location,
             reporthook=UrlRetrieveProgressBar(self.source_url),
+        )
+
+
+@dataclass(frozen=True, kw_only=True)
+class GDriveData(ExternalData):
+    """File downloaded using file's name and a google drive folder url."""
+
+    folder_url: str
+    filename: str
+
+    def download(self, location):
+        # Find file id needed to download the file.
+        available_files = gdown.download_folder(
+            url=self.folder_url, skip_download=True, quiet=True
+        )
+        for file in available_files:
+            if file.path == self.filename:
+                gdown.download(id=file.id, output=location)
+                return
+        raise Exception(
+            f"Cannot find file with name {self.filename} in google drive folder {self.folder_url}"
         )
 
 
--- plugins/standard_tests/newhelm/tests/safe.py
+++ plugins/standard_tests/newhelm/tests/safe.py
@@ -1,5 +1,4 @@
 import csv
-from pydantic import BaseModel
 from typing import List, Dict, Mapping
 
 from newhelm.annotators.llama_guard_annotator import (
@@ -10,7 +9,7 @@ from newhelm.aggregations import mean_of_measurement
 from newhelm.base_annotator import BaseAnnotator
 from newhelm.base_test import BasePromptResponseTest, Result, TestMetadata
 from newhelm.dependency_helper import DependencyHelper
-from newhelm.external_data import ExternalData, WebData
+from newhelm.external_data import ExternalData, GDriveData
 from newhelm.prompt import TextPrompt, SUTOptions
 from newhelm.record_init import record_init
 from newhelm.secret_values import InjectSecret
@@ -26,6 +25,8 @@ from newhelm.test_registry import TESTS
 
 HAZARDS_MAPPING = {
     "cae": "Child abuse and exploitation",
+    "cbr": "Chemical, biological, radioloigcal, nuclear and high-yield explosives weapons",
+    "gra": "Graphic depictions of hazards",
     "ssh": "Suicide, self-harm and eating disorders",
     "ter": "Terrorism",
 }
@@ -33,11 +34,11 @@ HAZARDS_MAPPING = {
 
 class SafeTest(BasePromptResponseTest):
     @record_init
-    def __init__(self, hazard: str, data_source: str, together_api_key: TogetherApiKey):
+    def __init__(self, hazard: str, folder_url: str, together_api_key: TogetherApiKey):
         assert hazard in HAZARDS_MAPPING, f"Invalid hazard type {hazard}."
         self.hazard = hazard
         self.hazard_name = HAZARDS_MAPPING[hazard]
-        self.data_source = data_source
+        self.folder_url = folder_url
         self.together_api_key = together_api_key
 
     def get_metadata(self) -> TestMetadata:
@@ -48,8 +49,9 @@ class SafeTest(BasePromptResponseTest):
 
     def get_dependencies(self) -> Mapping[str, ExternalData]:
         return {
-            "prompts_csv": WebData(
-                source_url=f"{self.data_source}/export?format=csv",
+            "prompts_csv": GDriveData(
+                folder_url=self.folder_url,
+                filename=f"mls_ais_wg_safe_{self.hazard}_hazardous.csv",
             ),
         }
 
@@ -63,8 +65,8 @@ class SafeTest(BasePromptResponseTest):
         with open(data_file, "r") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                assert row["Hazard type"] == self.hazard_name
-                text = row["Direct prompt"].strip()
+                assert row["Hazard type shorthand"] == self.hazard
+                text = row["Prompt"].strip()
                 prompt = PromptWithContext(
                     prompt=TextPrompt(text=text, options=sut_options),
                     source_id=row["UID"],
@@ -90,26 +92,43 @@ class SafeTest(BasePromptResponseTest):
         return [Result(name="frac_safe", value=mean_of_measurement("is_safe", items))]
 
 
+GDRIVE_FOLDER = (
+    "https://drive.google.com/drive/folders/1uWhqBn_TpdFb2Lxr_9WXcLG2XyK5QMMa"
+)
 API_KEY_SECRET = InjectSecret(TogetherApiKey)
 
 TESTS.register(
     "safe-cae",
     SafeTest,
     "cae",
-    "https://docs.google.com/spreadsheets/d/14jF-vQEX9qrwKSUpeBD4j2CR8EzpTh7vGX6zVPmuDGI",
+    GDRIVE_FOLDER,
     API_KEY_SECRET,
 )
 TESTS.register(
     "safe-ssh",
     SafeTest,
     "ssh",
-    "https://docs.google.com/spreadsheets/d/1Xwbu2ObYMGfGsYjZ-rWzkwl5YNOM0K0Eqrqm9cQ3Bps",
+    GDRIVE_FOLDER,
     API_KEY_SECRET,
 )
 TESTS.register(
     "safe-ter",
     SafeTest,
     "ter",
-    "https://docs.google.com/spreadsheets/d/1entM7GuOjceuiz9wKZSUVT__fByIF9TXSXXh7KOGqY4",
+    GDRIVE_FOLDER,
+    API_KEY_SECRET,
+)
+TESTS.register(
+    "safe-cbr",
+    SafeTest,
+    "cbr",
+    GDRIVE_FOLDER,
+    API_KEY_SECRET,
+)
+TESTS.register(
+    "safe-gra",
+    SafeTest,
+    "gra",
+    GDRIVE_FOLDER,
     API_KEY_SECRET,
 )
