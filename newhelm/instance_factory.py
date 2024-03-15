feb0b58449fac6688362b400e85c5f91420c96d4
Brian Goldman <auxy@google.com>
2024-03-15 14:22:29 -0600

    Have Tests and SUTs know their UIDs.
    While this touches a lot of files, the meaningful differences are really only:
    
    * InstanceFactory now assumes the first argument to everything it constructs is that object's UID.
    * run_prompt_response_test can read the UID directly from the test/sut objects.
    
    This is a step toward connecting the toml config to the Test instance.

--- newhelm/instance_factory.py
+++ newhelm/instance_factory.py
@@ -4,24 +4,26 @@ from typing import Any, Dict, Generic, List, Sequence, Tuple, Type, TypeVar
 from newhelm.dependency_injection import inject_dependencies
 
 from newhelm.secret_values import MissingSecretValues, RawSecrets
+from newhelm.tracked_object import TrackedObject
 
 
-_T = TypeVar("_T")
+_T = TypeVar("_T", bound=TrackedObject)
 
 
 @dataclass(frozen=True)
 class FactoryEntry(Generic[_T]):
     cls: Type[_T]
+    uid: str
     args: Tuple[Any]
     kwargs: Dict[str, Any]
 
     def __str__(self):
         """Return a string representation of the entry."""
-        return f"{self.cls.__name__}(args={self.args}, kwargs={self.kwargs})"
+        return f"{self.cls.__name__}(uid={self.uid}, args={self.args}, kwargs={self.kwargs})"
 
     def make_instance(self, *, secrets: RawSecrets) -> _T:
         args, kwargs = inject_dependencies(self.args, self.kwargs, secrets=secrets)
-        return self.cls(*args, **kwargs)  # type: ignore [call-arg]
+        return self.cls(self.uid, *args, **kwargs)  # type: ignore [call-arg]
 
     def get_missing_dependencies(
         self, *, secrets: RawSecrets
@@ -41,36 +43,36 @@ class InstanceFactory(Generic[_T]):
         self._lookup: Dict[str, FactoryEntry[_T]] = {}
         self.lock = threading.Lock()
 
-    def register(self, key: str, cls: Type[_T], *args, **kwargs):
+    def register(self, cls: Type[_T], uid: str, *args, **kwargs):
         """Add value to the registry, ensuring it has a unique key."""
         with self.lock:
-            previous = self._lookup.get(key)
+            previous = self._lookup.get(uid)
             assert previous is None, (
-                f"Factory already contains {key} set to "
+                f"Factory already contains {uid} set to "
                 f"{previous.cls.__name__}(args={previous.args}, "
                 f"kwargs={previous.kwargs})."
             )
-            self._lookup[key] = FactoryEntry[_T](cls, args, kwargs)
+            self._lookup[uid] = FactoryEntry[_T](cls, uid, args, kwargs)
 
-    def make_instance(self, key: str, *, secrets: RawSecrets) -> _T:
+    def make_instance(self, uid: str, *, secrets: RawSecrets) -> _T:
         """Create an instance using the  class and arguments passed to register, raise exception if missing."""
-        entry = self._get_entry(key)
+        entry = self._get_entry(uid)
         return entry.make_instance(secrets=secrets)
 
     def get_missing_dependencies(
-        self, key: str, *, secrets: RawSecrets
+        self, uid: str, *, secrets: RawSecrets
     ) -> Sequence[MissingSecretValues]:
-        entry = self._get_entry(key)
+        entry = self._get_entry(uid)
         return entry.get_missing_dependencies(secrets=secrets)
 
-    def _get_entry(self, key: str) -> FactoryEntry:
+    def _get_entry(self, uid: str) -> FactoryEntry:
         with self.lock:
             entry: FactoryEntry
             try:
-                entry = self._lookup[key]
+                entry = self._lookup[uid]
             except KeyError:
-                known_keys = list(self._lookup.keys())
-                raise KeyError(f"No registration for {key}. Known keys: {known_keys}")
+                known_uids = list(self._lookup.keys())
+                raise KeyError(f"No registration for {uid}. Known uids: {known_uids}")
         return entry
 
     def items(self) -> List[Tuple[str, FactoryEntry[_T]]]:
