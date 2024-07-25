3449d86b1d2b875a56d1356de207ab905c382aef
william <william@williampietri.com>
2024-07-25 13:45:30 -0500

    Add annotations.

--- modelgauge/api_server.py
+++ modelgauge/api_server.py
@@ -1,17 +1,20 @@
 import itertools
 import multiprocessing
-import time
-from typing import Sequence
+from typing import Sequence, Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from annotator import CompletionAnnotator
+from modelgauge.annotator_registry import ANNOTATORS
+from modelgauge.annotators.llama_guard_annotator import LlamaGuardAnnotator
 from modelgauge.config import load_secrets_from_config
 from modelgauge.load_plugins import load_plugins
 from modelgauge.sut import PromptResponseSUT
 from modelgauge.sut_registry import SUTS
 from modelgauge.suts.together_client import CHAT_MODELS
-from prompt import TextPrompt
+from modelgauge.prompt import TextPrompt
+from single_turn_prompt_response import PromptWithContext
 
 """
   Simple API server for modelgauge functionality. Currently used just for interviews.
@@ -39,12 +42,17 @@ suts: dict[str, PromptResponseSUT] = {
     for sut_uid in CHAT_MODELS.keys()
 }
 
-print(f"got suts {suts}")
+annotators: dict[str, CompletionAnnotator] = {
+    sut_uid: ANNOTATORS.make_instance(sut_uid, secrets=secrets)
+    for sut_uid in [i[0] for i in ANNOTATORS.items()]
+}
 
+print(f"got suts {suts} and annotators {annotators}")
 
 class ProcessingRequest(BaseModel):
     prompts: Sequence[TextPrompt]
     suts: Sequence[str]
+    annotators: Sequence[str] = []
 
 
 app = FastAPI()
@@ -52,20 +60,28 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"suts": list(suts.keys())}
+    return {"suts": list(suts.keys()), "annotators": list(annotators.keys())}
 
 
-def process_work_item(prompt: TextPrompt, sut: PromptResponseSUT):
-    start = time.time()
-    request = sut.translate_text_prompt(prompt)
-    response = sut.evaluate(request)
-    print(f"{sut.uid} took {time.time() - start}")
-    return {"sut": sut.uid, "response": sut.translate_response(request, response)}
+def process_work_item(prompt: TextPrompt, sut_key: str , annotator_key: str = None):
+    sut = suts[sut_key]
+    s_req = sut.translate_text_prompt(prompt)
+    s_resp = sut.translate_response(s_req, sut.evaluate(s_req))
+    result = {"sut": sut.uid, "response": s_resp}
+    if annotator_key:
+        annotator = annotators[annotator_key]
+        a_req = annotator.translate_request(PromptWithContext(prompt=prompt, source_id="whatever, man"), s_resp.completions[0])
+        result["annotation"] = annotator.translate_response(a_req, annotator.annotate(a_req))
+    return result
 
 
 @app.post("/")
 async def postroot(req: ProcessingRequest):
-    work_items = list(itertools.product(req.prompts, [suts[k] for k in req.suts]))
+    if req.annotators:
+        work_items = list(itertools.product(req.prompts, req.suts, req.annotators))
+    else:
+        work_items = list(itertools.product(req.prompts, req.suts))
+
     print(work_items)
     pool = multiprocessing.pool.ThreadPool(len(work_items))
     results = pool.starmap(process_work_item, work_items)
