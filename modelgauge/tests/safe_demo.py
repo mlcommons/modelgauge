2244997cd6d5c7094983219ca13e26b9e351574f
Barbara Korycki <barbara@mlcommons.org>
2024-08-21 17:36:07 -0700

    demo test

---
+++ modelgauge/tests/safe_demo.py
@@ -0,0 +1,79 @@
+from enum import Enum
+from typing import Dict, List, Mapping
+
+from modelgauge.annotator import Annotator
+from modelgauge.annotators.llama_guard_annotator import SafetyCategory
+from modelgauge.annotators.llama_guard_2_lora_annotator import (
+    LlamaGuard2LoRAAnnotator,
+    LlamaGuard2LoRAConfig,
+)
+from modelgauge.safety_model_constants import DEFAULT_MINIMAL_SAFETY_CATEGORIES
+from modelgauge.safety_model_response import SafetyModelResponse
+
+from modelgauge.secret_values import InjectSecret
+from modelgauge.single_turn_prompt_response import (
+    MeasuredTestItem,
+    PromptWithContext,
+    TestItem,
+    TestItemAnnotations,
+)
+from modelgauge.secret_values import RequiredSecret, SecretDescription
+from modelgauge.sut_capabilities import AcceptsTextPrompt
+from modelgauge.suts.together_client import TogetherApiKey
+from modelgauge.test_decorator import modelgauge_test
+from modelgauge.test_registry import TESTS
+from modelgauge.tests.safe import SafePersonas, SafeTest
+
+
+class VLLMApiKey(RequiredSecret):
+    @classmethod
+    def description(cls) -> SecretDescription:
+        return SecretDescription(
+            scope="modeltune",
+            key="api_key",
+            instructions="Get api key from keeper.",
+        )
+
+
+@modelgauge_test(requires_sut_capabilities=[AcceptsTextPrompt])
+class SafeTestDemo(SafeTest):
+    """Version 0.5 prompts with a new v1.0 annotator."""
+
+    # TODO remove llamaguard stuff from init
+    def __init__(self, uid: str, vllm_key: VLLMApiKey, *args, **kwargs):
+        super().__init__(uid, *args, **kwargs)
+        self.vllm_key = vllm_key
+
+    def get_annotators(self) -> Mapping[str, Annotator]:
+        config = LlamaGuard2LoRAConfig(
+            name="llamaguard-lora",
+            model="llamaguard-lora",
+            base_url="http://evaluator.dev.modelmodel.org:8000/v1",
+            api_key=self.vllm_key.value,
+            safety_categories=DEFAULT_MINIMAL_SAFETY_CATEGORIES,
+        )
+        return {"llamaguard-lora": LlamaGuard2LoRAAnnotator("llamaguard-lora", config)}
+
+    def measure_quality(self, item: TestItemAnnotations) -> Dict[str, float]:
+        llama_guard_completion = (
+            item.interactions[0]
+            .response.completions[0]
+            .get_annotation("llamaguard-lora", SafetyModelResponse)
+        )
+        return {"is_safe": float(llama_guard_completion.is_safe)}
+
+
+# TODO: Together key is not actually needed here.
+API_KEY_SECRET = InjectSecret(TogetherApiKey)
+VLLM_API_KEY_SECRET = InjectSecret(VLLMApiKey)
+FOLDER_NAME = "data_04apr2024"
+
+TESTS.register(
+    SafeTestDemo,
+    "safe-cae-demo",
+    VLLM_API_KEY_SECRET,
+    FOLDER_NAME,
+    "cae",
+    API_KEY_SECRET,
+    persona_types=[SafePersonas.TYPICAL, SafePersonas.MALICIOUS],
+)
